ecd814dc45a8fef65651ab9fb3c643828fe81e0d
Anders Rune Jensen <arj03@protonmail.ch>
2021-05-07 00:10:23 +0200

    Use derive-key as an example

--- keys.js
+++ keys.js
@@ -1,41 +1,24 @@
 const crypto = require("crypto")
-// use https://www.npmjs.com/package/derive-key instead
-const hkdf = require('futoin-hkdf')
 const ssbKeys = require('ssb-keys')
+const derive = require('derive-key')
 
+/*
 const length = 32
-
 const ikm = crypto.randomBytes(length) // aka as seed
+*/
 
 // or from backup:
-/*
-const ikm_hex = '1fd53bb951cc1ca41d1efce5c2710bc0b61c32c8fff6b774166490fc086254c9'
-const ikm = Buffer.from(ikm_hex, 'hex')
-*/
+const ikm_hex = '4e2ce5ca70cd12cc0cee0a5285b61fbc3b5f4042287858e613f9a8bf98a70d39'
+ikm = Buffer.from(ikm_hex, 'hex')
 
 console.log("seed", ikm.toString('hex'))
 
-const salt = 'ssbc'
-const hash = 'SHA-256'
-
-const lhash  = 'sha256'
-const hash_len = hkdf.hash_length(lhash)
-const prk = hkdf.extract(lhash, hash_len, ikm, salt)
-
-const mf_info = "ssb-meta-feed-seed-v1:metafeed"
-//const mf_seed = hkdf(ikm, length, {salt, mf_info, hash})
-const mf_seed = hkdf.expand(lhash, hash_len, prk, length, mf_info);
-
-console.log("mf seed", mf_seed)
-
+const mf_seed = derive('ssb-meta-feed', ikm, 'ssb-meta-feed-seed-v1:metafeed')
+//console.log('the derived meta feed seed is:', mf_seed)
 const mf_key = ssbKeys.generate("ed25519", mf_seed)
 console.log("mf_key", mf_key)
 
-const sf_info = "ssb-meta-feed-seed-v1:subfeed-1"
-
-const sf_seed = hkdf.expand(lhash, hash_len, prk, length, sf_info);
-
-console.log("sf_seed", sf_seed)
-
+const sf_seed = derive('ssb-meta-feed', ikm, 'ssb-meta-feed-seed-v1:subfeed-1')
+//console.log('the derived sub feed seed is:', sf_seed)
 const sf_key = ssbKeys.generate("ed25519", sf_seed)
 console.log("sf_key", sf_key)
